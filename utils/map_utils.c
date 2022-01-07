b3a02e3a0beaff34bcf1e737debbdfd5c85d988f
julioakira <julio.akira.pedroso@gmail.com>
2022-01-07 06:38:45 -0300

    better organization w/ wrapper functions

--- utils/map_utils.c
+++ utils/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: jakira-p <jakira-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 00:29:14 by jakira-p          #+#    #+#             */
-/*   Updated: 2022/01/07 05:16:18 by jakira-p         ###   ########.fr       */
+/*   Updated: 2022/01/07 06:38:10 by jakira-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,48 +22,65 @@ static int	measure_enclosing(int width, int height)
 }
 
 // smallest possible size (5x3)
-static int	check_min_size(t_map *map)
+static void	check_map_size(t_map *map)
 {
 	if (map->width < 5)
 	{
 		if (map->height <= 3)
-			return (0);
+		{
+			free_map(map);
+			exit_and_print(EINVAL,
+				"Error: Map is smaller than the minimum size (5x3).\n");
+		}
 	}
 	if (map->height < 3)
 	{
 		if (map->width <= 5)
-			return (0);
+		{
+			free_map(map);
+			exit_and_print(EINVAL,
+				"Error: Map is smaller than the minimum size (5x3).\n");
+		}
 	}
-	return (1);
+}
+
+static void	check_map_elements(t_map *map)
+{
+	if (map->exit != 1)
+	{
+		free_map(map);
+		exit_and_print(EINVAL,
+			"Error: Map must have only one exit.\n");
+	}
+	if (map->collectibles == 0)
+	{
+		free_map(map);
+		exit_and_print(EINVAL,
+			"Error: Map must have at least one collectible.\n");
+	}
+	if (map->start != 1)
+	{
+		free_map(map);
+		exit_and_print(EINVAL,
+			"Error: Map must have only one player spawn.\n");
+	}
+}
+
+static void	check_map_enclosing(t_map *map)
+{
+	if (measure_enclosing(map->width, map->height) != map->enclosing_walls)
+	{
+		free_map(map);
+		exit_and_print(EINVAL,
+			"Error: Map must be wall enclosed.\n");
+	}
 }
 
 // Check again later if all validations are being applied
 void	is_valid_map(t_map *map)
 {
 	parse_map_elements(map);
-	if (check_min_size(map) == 0)
-	{
-		free_map(map);
-		exit_and_print(EINVAL, "Error: Map is smaller than the minimum size (5x3).\n");
-	}
-	if (map->exit != 1)
-	{
-		free_map(map);
-		exit_and_print(EINVAL, "Error: Map must have only one exit.\n");
-	}
-	if (map->collectibles == 0)
-	{
-		free_map(map);
-		exit_and_print(EINVAL, "Error: Map must have at least one collectible.\n");
-	}
-	if (map->start != 1)
-	{
-		free_map(map);
-		exit_and_print(EINVAL, "Error: Map must have only one player spawn.\n");
-	}
-	if (measure_enclosing(map->width, map->height) != map->enclosing_walls)
-	{
-		free_map(map);
-		exit_and_print(EINVAL, "Error: Map must be wall enclosed.\n");
-	}
+	check_map_size(map);
+	check_map_elements(map);
+	check_map_enclosing(map);
 }
